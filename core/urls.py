744ca092cdc149ae73794776f2fa37f92a94864e
sarina <sarinaomidvar99@gmail.com>
2023-10-07 15:09:22 +0330

    some changes on template

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/' , include('polls.urls')),
+    path('' , include('polls.urls')),
     #path('' , include('pages.urls')),
     
 ]
